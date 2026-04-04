1b96cbe10f91ffa4878f284ceb036475a2b2cb48
Tom Depasse <tom.depasse@gmail.com>
2026-04-04 19:17:35 +0200

    config: eslint – set file naming conventions to kebab-case

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,11 @@
-import * as js from "@eslint/js";
-import * as globals from "globals";
+import js from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-import * as pluginReact from "eslint-plugin-react";
+import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
-import * as eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import checkFile from "eslint-plugin-check-file";
 
 export default defineConfig([
   {
@@ -15,5 +16,36 @@ export default defineConfig([
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  /* PRETTIER */
   eslintConfigPrettier,
+  /* NAMING CONVENTIONS */
+  {
+    plugins: {
+      "check-file": checkFile,
+    },
+    rules: {
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          // ignore the middle extensions of the filename to support filename like bable.config.js or smoke.spec.ts
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          // all folders within src should be named in kebab-case
+          "src/**/!(app)": "KEBAB_CASE",
+        },
+      ],
+    },
+  },
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ]);

fa2b22dfbaeca1fef5ee62541da105f24dfb7243
Tom Depasse <tom.depasse@gmail.com>
2026-04-04 15:38:55 +0200

    config: setup eslint

---
+++ eslint.config.ts
@@ -0,0 +1,17 @@
+import * as js from "@eslint/js";
+import * as globals from "globals";
+import tseslint from "typescript-eslint";
+import * as pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js, unicorn: eslintPluginUnicorn },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);

be3ea0479da30db2cddbca5f62b9feb5c44e8fca
Tom Depasse <tom.depasse@gmail.com>
2026-04-06 16:38:01 +0200

    feat: home page – link to riddles and jokes routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 import { CategoryCard } from "@/components/category-card.tsx";
 
 export const Route = createFileRoute("/")({
@@ -14,34 +14,38 @@ function HomePage() {
         <span className="underline text-emerald-500">Blagues</span> ?
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12">
-        <CategoryCard
-          icon={<span className="text-2xl">🤔</span>}
-          title="Devinettes"
-          description="Ici, même les réponses te regardent en mode “c’était pourtant évident”."
-          bgClassName="bg-indigo-400"
-          cardsContent={
-            <div className="text-lg text-gray-400 font-sans text-center">
-              Il y a 7 nains autour d&apos;une table. Sur cette table, il y a 7
-              assiettes, 7 couverts, ...
-            </div>
-          }
-        />
-        <CategoryCard
-          icon={<span className="text-2xl">🤣</span>}
-          title="Blagues"
-          description="Ici, le niveau descend parfois très bas… mais le sourire monte toujours très vite."
-          bgClassName="bg-emerald-400"
-          cardsContent={
-            <div className="flex flex-col justify-center">
-              <span className="text-lg text-gray-400 font-sans text-center">
-                Qu&apos;est-ce qui est jaune et qui attend ?
-              </span>
-              <span className="text-lg text-gray-500 font-sans text-center">
-                Jonathan
-              </span>
-            </div>
-          }
-        />
+        <Link to="/riddles">
+          <CategoryCard
+            icon={<span className="text-2xl">🤔</span>}
+            title="Devinettes"
+            description="Ici, même les réponses te regardent en mode “c’était pourtant évident”."
+            bgClassName="bg-indigo-400"
+            cardsContent={
+              <div className="text-lg text-gray-400 font-sans text-center">
+                Il y a 7 nains autour d&apos;une table. Sur cette table, il y a
+                7 assiettes, 7 couverts, ...
+              </div>
+            }
+          />
+        </Link>
+        <Link to="/jokes">
+          <CategoryCard
+            icon={<span className="text-2xl">🤣</span>}
+            title="Blagues"
+            description="Ici, le niveau descend parfois très bas… mais le sourire monte toujours très vite."
+            bgClassName="bg-emerald-400"
+            cardsContent={
+              <div className="flex flex-col justify-center">
+                <span className="text-lg text-gray-400 font-sans text-center">
+                  Qu&apos;est-ce qui est jaune et qui attend ?
+                </span>
+                <span className="text-lg text-gray-500 font-sans text-center">
+                  Jonathan
+                </span>
+              </div>
+            }
+          />
+        </Link>
       </div>
     </>
   );

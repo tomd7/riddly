b7fac5d286dbd7117620615c2297524e6801a831
Tom Depasse <tom.depasse@gmail.com>
2026-04-06 15:39:08 +0200

    feat: home page – cards content update

--- src/layout/app.tsx
+++ src/layout/app.tsx
@@ -15,6 +15,12 @@ export default function App() {
             title="Devinettes"
             description="Ici, même les réponses te regardent en mode “c’était pourtant évident”."
             bgClassName="bg-indigo-400"
+            cardsContent={
+              <div className="text-lg text-gray-400 font-sans text-center">
+                Il y a 7 nains autour d&apos;une table. Sur cette table, il y a
+                7 assiettes, 7 couverts, ...
+              </div>
+            }
           />
           <CategoryCard
             icon={<span className="text-2xl">🤣</span>}
@@ -22,8 +28,13 @@ export default function App() {
             description="Ici, le niveau descend parfois très bas… mais le sourire monte toujours très vite."
             bgClassName="bg-emerald-400"
             cardsContent={
-              <div className="text-lg text-gray-400 font-sans text-center">
-                Qu&apos;est-ce qui est jaune et qui attend ?
+              <div className="flex flex-col justify-center">
+                <span className="text-lg text-gray-400 font-sans text-center">
+                  Qu&apos;est-ce qui est jaune et qui attend ?
+                </span>
+                <span className="text-lg text-gray-500 font-sans text-center">
+                  Jonathan
+                </span>
               </div>
             }
           />

7e9502d47268b00aab8c87f544fc2140ce968d7b
Tom Depasse <tom.depasse@gmail.com>
2026-04-19 20:57:24 +0200

    feat: fetch riddles

--- src/config/pb.ts
+++ src/config/pb.ts
@@ -3,20 +3,12 @@ import PocketBase from "pocketbase";
 const pbClient = new PocketBase(import.meta.env.VITE_API_URL);
 
 function authenticate() {
-  return (
-    pbClient
-      .collection("_superusers")
-      // TODO : extraire dans .env
-      .authWithPassword(
-        import.meta.env.VITE_PB_USERNAME,
-        import.meta.env.VITE_PB_PASSWORD,
-      )
-  );
-}
-
-export async function pbRequest() {
-  await authenticate();
-  return pbClient;
+  return pbClient
+    .collection("_superusers")
+    .authWithPassword(
+      import.meta.env.VITE_PB_USERNAME,
+      import.meta.env.VITE_PB_PASSWORD,
+    );
 }
 
 async function makeApi() {

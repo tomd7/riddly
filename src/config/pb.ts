cca31137afec52905ed8424ac9a62e644ea2f597
Tom Depasse <tom.depasse@gmail.com>
2026-05-08 20:49:36 +0200

    refactor: pocketbase instance

--- src/config/pb.ts
+++ src/config/pb.ts
@@ -1,9 +1,3 @@
 import PocketBase from "pocketbase";
 
-const pbClient = new PocketBase(import.meta.env.VITE_PB_URL);
-
-async function makeApi() {
-  return pbClient;
-}
-
-export const pb = await makeApi();
+export const pb = new PocketBase(import.meta.env.VITE_PB_URL);

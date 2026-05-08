f834fcdd5acac7c7144f03557488361ad6e6ccde
Tom Depasse <tom.depasse@gmail.com>
2026-05-08 17:45:48 +0200

    fix: pocketbase security

--- src/config/pb.ts
+++ src/config/pb.ts
@@ -1,18 +1,8 @@
 import PocketBase from "pocketbase";
 
-const pbClient = new PocketBase(import.meta.env.VITE_API_URL);
-
-function authenticate() {
-  return pbClient
-    .collection("_superusers")
-    .authWithPassword(
-      import.meta.env.VITE_PB_USERNAME,
-      import.meta.env.VITE_PB_PASSWORD,
-    );
-}
+const pbClient = new PocketBase(import.meta.env.VITE_PB_URL);
 
 async function makeApi() {
-  await authenticate();
   return pbClient;
 }
 
